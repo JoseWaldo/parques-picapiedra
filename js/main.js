79d1b9b06df77ec20d26265d0c753d63eca6383b
Jose Waldo <josferw@gmail.com>
2022-06-12 19:35:47 -0500

    Creacion de casillas, funciona este método

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-const parques = document.querySelector('#parques')
+const parques = document.querySelector('.juego__parques')
+const fichaRoja = document.createElement('div')
+const fichaAzul = document.createElement('div')
+const fichaVerde = document.createElement('div')
+
 
 parques.addEventListener('click', (e) => {
-    console.log(e.target.id)
+    if(e.target.id === 'casilla-prueba') {
+        const casillaPrueba = document.querySelector('#casilla-prueba')
+        fichaRoja.id = 'ficha-roja'
+        fichaAzul.id = 'ficha-azul'
+        fichaVerde.id = 'ficha-verde'
+
+        casillaPrueba.insertAdjacentElement('afterbegin', fichaRoja)
+        casillaPrueba.insertAdjacentElement('afterbegin', fichaAzul)
+        casillaPrueba.insertAdjacentElement('afterbegin', fichaVerde)
+    }
 }, false);
